380bc6f77c3a777f751fb6ea5be13467242b353c
kianoushit1999 <kianoushit1999@gmail.com>
2024-06-27 16:35:49 +0330

    adding styles by clicking over different options which shows correct option and wrong ones

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,9 +1,17 @@
-function Options({children}) {
-    return (
-        <div className="options">
-            <button className="btn btn-option">{children}</button>
-        </div>
-    )
+function Options({ children, ind, correctAnswerHandler, selectedAns, ans }) {
+  return (
+    <div className="options">
+      <button
+        className={`btn btn-option ${ind === selectedAns && "answer"} ${
+          selectedAns !== null ? (ind === ans ? "correct" : "wrong") : ""
+        }`}
+        onClick={() => correctAnswerHandler(ind)}
+        disabled={selectedAns !== null}
+      >
+        {children}
+      </button>
+    </div>
+  );
 }
 
 export default Options;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,21 @@
 
+import { useState } from 'react';
 import Options from './Options';
 
 function Question({qInfo}) {
+
+    const ans = qInfo.correctOption
+    const [selectedAns, setSelectedAns] = useState(null);
+
+    function selectedAnsHandler(id) {
+        setSelectedAns(id)
+    }
+
     return (
         <div>
             <h4>{qInfo.question}</h4>
             {qInfo.options.map((value, index, array) => {
-                return <Options key={index}>{value}</Options>
+                return <Options key={index} ind={index} correctAnswerHandler={selectedAnsHandler} selectedAns={selectedAns} ans={ans}>{value}</Options>
             })}
         </div>
     )
